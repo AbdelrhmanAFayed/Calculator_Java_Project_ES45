768018f7d2eb8cf0957a3f620acb9de495183b62
Mariam <mariam.hossam.keshk@gmail.com>
2025-04-05 22:56:27 +0200

    Fix: removed module-info.java

--- Java_Module/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.java_module {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens com.mycompany.java_module to javafx.fxml;
-    exports com.mycompany.java_module;
-}
